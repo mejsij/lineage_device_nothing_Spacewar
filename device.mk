PixelExperience-Devices/device_nothing_Spacewar

Spacewar: Switch to QTI health HAL implementation
 * Fixed the battery meter not working properly.

Change-Id: Ie17b01135fdd311ca512da6997dc875894ea5458

device.mk
@@ -229,7 +229,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl-qti \
     android.hardware.health@2.1-service
 
 # HIDL

mejsij/lineage_device_nothing_Spacewar

 * Import from LA.UM.9.14.r1-21000-LAHAINA.QSSI13.0.
Signed-off-by: aswin7469 <aswinas@pixysos.com>

device.mk
@@ -229,7 +229,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl-Spacewar \
     android.hardware.health@2.1-service
 
 # HIDL