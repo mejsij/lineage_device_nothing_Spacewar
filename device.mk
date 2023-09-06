d9924c60e5773bef2733f6301404558631684c86
mejsij <maciek00792@o2.pl>
2023-09-06 21:27:48 +0200

    Spacewar: Include Nothing Camera

--- device.mk
+++ device.mk
@@ -455,3 +455,6 @@ PRODUCT_BOOT_JARS += \
 
 # Firmware 
 $(call inherit-product, vendor/nothing/firmware/Android.mk)
+
+# NT Camera
+$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
